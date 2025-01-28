67d6e37073f75b8df49a8342c573a888839ffab5
RSpace <124686065+RSpaced@users.noreply.github.com>
2025-01-28 13:16:29 +0000

    Add files via upload

---
+++ _vti_cnf/animate.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Alex
+vti_modifiedby:SR|Alex
+vti_timecreated:TR|21 Jan 2025 19:44:17 -0000
+vti_timelastmodified:TR|21 Jan 2025 19:44:17 -0000
+vti_cacheddtm:TX|21 Jan 2025 19:44:17 -0000
+vti_filesize:IR|14261
+vti_extenderversion:SR|4.0.2.8912
+vti_backlinkinfo:VX|
